8ef61dcc086aac79bb11d9c2d5bccaff892bc892
Tiago Ribeiro Navarro de Andrade <42575083+tiagornandrade@users.noreply.github.com>
2024-11-18 21:36:56 -0300

    chore: Estruturar projeto

--- api-gateway/app/main.py
+++ api-gateway/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Depends
 from auth import JWTBearer
 from routers import drivers, rides, locations, pricings
+from middlewares import LoggingMiddleware, AddHeadersMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Mobility API Gateway")
 Instrumentator().instrument(app).expose(app)
+
+app.add_middleware(LoggingMiddleware)
+app.add_middleware(AddHeadersMiddleware)
 
 app.include_router(drivers.router, prefix="/drivers", dependencies=[Depends(JWTBearer())])
 app.include_router(rides.router, prefix="/rides", dependencies=[Depends(JWTBearer())])
